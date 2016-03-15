a06c4e908b81c218c29ae40a8ad84eaefc4ae4f0
SeiYukinari <s_yukinari@e-create.jp>
2016-03-15 10:54:48 +0900

    キャッシュの時間変更

--- module/Application/src/Application/Domain/Infrastracture/Cache.php
+++ module/Application/src/Application/Domain/Infrastracture/Cache.php
@@ -11,7 +11,7 @@ class Cache
      * @param int $ttl
      * @return Filesystem
      */
-    public static function getAdapter($ttl = 3600)
+    public static function getAdapter($ttl = 10800)
     {
         $cache = new Filesystem();
         $cache->getOptions()->setTtl($ttl);
